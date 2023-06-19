b51ec49851a84d62e5c4aea35ef8c882d06c6167
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-06-19 18:45:10 -0300

    Restauran Interface

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Header = ({ textH }) => {
+  return (
+    <View>
+      <Text >{textH}</Text>
+    </View>
+  );
+};
