baaf3291af59200d6ae84f8e24d626242c85dac9
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-06-19 19:09:54 -0300

    Estado cierre

--- src/components/Restaurante Interface/interface.jsx
+++ src/components/Restaurante Interface/interface.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 
 const RestaurantInterface = () =>{
-    // Estado para el horario de apertura
+    // Estado para el horario de apertura y cierre
     const [apertura, setapertura] = useState ('');
+    const [cierre, setcierre] = useState('');
 }
