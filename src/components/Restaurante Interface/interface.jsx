438177784218f628015199236087cfcb82ad4b91
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-06-19 19:11:43 -0300

    Estado promociones

--- src/components/Restaurante Interface/interface.jsx
+++ src/components/Restaurante Interface/interface.jsx
@@ -4,4 +4,7 @@ const RestaurantInterface = () =>{
     // Estado para el horario de apertura y cierre
     const [apertura, setapertura] = useState ('');
     const [cierre, setcierre] = useState('');
+
+    // Estado para las promociones y envios gratis
+    const [promo, setpromo] = useState('');
 }
