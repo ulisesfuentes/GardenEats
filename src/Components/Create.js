11fbf354a0bc8dd9349f985f48ee41c7a64e03b5
ulisesfuentes <93626190+ulisesfuentes@users.noreply.github.com>
2023-06-20 18:14:41 -0300

    creo  funcion create

---
+++ src/Components/Create.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { collection, addDoc } from 'firebase/firestore'
+import { db } from '../firebaseConfig/firebase'
+
+const Create = () => {
+  const [ description, setDescription ] = useState('')
+  const [ stock, setStock ] = useState(0)
+  const navigate = useNavigate()
+}
