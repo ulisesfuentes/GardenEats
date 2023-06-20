fa5d0906030ff3d36a2052dccfed14723d86cdf9
ulisesfuentes <93626190+ulisesfuentes@users.noreply.github.com>
2023-06-20 18:17:46 -0300

    agrego return

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -15,5 +15,37 @@ const Create = () => {
     await addDoc( productsCollection, { description: description, stock: stock } )
     navigate('/')
     //console.log(e.target[0].value)
-  } 
 }
+return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col'>
+                <h1>Create Product</h1>
+                 <form onSubmit={store}>
+                    <div className='mb-3'>
+                        <label className='form-label'>Description</label>
+                        <input
+                            value={description}
+                            onChange={ (e) => setDescription(e.target.value)} 
+                            type="text"
+                            className='form-control'
+                        />
+                    </div>  
+                    <div className='mb-3'>
+                        <label className='form-label'>Stock</label>
+                        <input
+                            value={stock}
+                            onChange={ (e)=> setStock(e.target.value)} 
+                            type="number"
+                            className='form-control'
+                        />                 
+                    </div>  
+                    <button type='submit' className='btn btn-primary'>Store</button>
+                 </form>   
+            </div>
+        </div>
+    </div> 
+  )
+}
+
+export default Create
