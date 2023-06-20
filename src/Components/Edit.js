4018c3ca839dc8d8512bf8bad2c5b142d4032e56
ulisesfuentes <93626190+ulisesfuentes@users.noreply.github.com>
2023-06-20 18:09:37 -0300

    creo la funcion edit

---
+++ src/Components/Edit.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { getDoc, updateDoc, doc } from "firebase/firestore"
+import { db } from "../firebaseConfig/firebase"
+
+const Edit = () => {
+    const [ description, setDescription ] = useState('')
+    const [ stock, setStock ] = useState(0)
+
+    const navigate = useNavigate()    
+    const {id} = useParams()
+
+    const update = async (e) => {
+        e.preventDefault()
+        const product = doc(db, "products", id)
+        const data = {description: description, stock: stock}
+        await updateDoc(product, data)
+        navigate('/')
+    }
+
+}
