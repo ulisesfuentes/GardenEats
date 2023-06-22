7a5ef2b344628dbfa5185e04f16f68b36e4a5148
Haiquel Lawreniuk <131982318+haiquellawreniuk@users.noreply.github.com>
2023-06-22 12:27:08 -0300

    Implementación menú de opciones

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Mercados from './src/components/Mercados/Mercados';
 import Header from './src/components/Header/Header.jsx';
 import logohome from './src/Imagenes/gardenlogo1.png'
+import logo from './src/Imagenes/iconMenu.png'
 import carniceria from './src/Imagenes/carniceria.png'
 import verduleria from './src/Imagenes/logo.jpg'
 import './App.css';
@@ -10,7 +11,7 @@ function App () {
   return (
     <div className="App">
 
-<Header img={logohome}/>
+<Header img={logohome} logo={logo}/>
 
     <Mercados img={carniceria} nombreMercado="Carniceria- Fresh Meal" horarioMercado="22:00" entregaMercado="Con delivery" modoPago="Online"/>
     
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-function Header(props){
-    return (
-        <div className="header">
-          <img className='logohome' src={props.img} alt="logo" />
-        </div>
-    )
+function Header(props) {
+  return (
+    <div className="header">
+      <img className="logohome" src={props.img} alt="logo" />
+      <img className="menu" src={props.logo} alt="menu" />
+    </div>
+  );
 }
+
 
 export default Header;
