40b3451aa3897520ee9f6449f4d7bbeca76b0506
Facundo7Rodriguez <rodriguezfacundo259@gmail.com>
2023-06-25 16:42:09 -0300

    Funciones nombre, precio, descripcion

--- src/components/New Product/producto.jsx
+++ src/components/New Product/producto.jsx
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
-const FormularioNuevoProducto = (){
-    
+const FormularioNuevoProducto = () =>{
+    const [nombre, setName] = useState ("");
+    const [precio, setPrecio] = useState ("");
+    const [descripcion, setDescripcion ] = useState ("");
+
+    const enviar = (e) =>{
+        e.prevenir();
+    }
 }
